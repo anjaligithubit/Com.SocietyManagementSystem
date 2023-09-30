d5ed8ea4f53758e44e6715fb18d235647828bb68
anjaligithubit <anjalijena66@gmail.com>
2023-09-30 16:57:30 +0530

    Files added

--- src/test/java/com/sms/TestCases/SampleTest.java
+++ src/test/java/com/sms/TestCases/SampleTest.java
@@ -11,5 +11,8 @@ public class SampleTest {
 	public void sample1() {
 		System.out.println("--- sample1 ---");
 	}
+	@Test public void sample2() {
+		System.out.println("--- sample2 ----");
+	}
 
 }
